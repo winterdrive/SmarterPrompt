71156dfef6570f9ca136b1d44ebfdc14eb9b212b
winterdrive <90021888+winterdrive@users.noreply.github.com>
2024-10-06 04:45:05 +0800

    [NER_frontend] fetch NER api

--- NER_frontend/content.js
+++ NER_frontend/content.js
@@ -46,12 +46,12 @@ document.addEventListener('mouseup', function (event) {
         // 點擊泡泡後呼叫 API 並顯示對話框
         bubble.addEventListener('click', function () {
             console.log(selectedText);
-            fetch('http://127.0.0.1:5000/reverse', {
+            fetch('http://127.0.0.1:5000/mask', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ string: selectedText })
+                body: JSON.stringify({string: selectedText})
             })
                 .then(response => response.json())
                 .then(data => {
@@ -78,13 +78,19 @@ document.addEventListener('mouseup', function (event) {
                     dialogBox.style.minWidth = '200px';  // 限制最小寬度
                     dialogBox.style.minHeight = '100px'; // 限制最小高度
                     dialogBox.style.overflow = 'auto'; // 超出顯示滾動條
-                    dialogBox.innerText = data.reversed_string;
+                    dialogBox.innerText = data.masked_text;
 
-                    // 創建複製按鈕
+                    for (mask_entity of data.masked_entities) {
+                        console.log(mask_entity['label']);
+                        console.log(mask_entity['mask']);
+                        console.log(mask_entity['original_value']);
+                        // console.log(mask_entity.original_value);
+                    }
+
                     let copyButton = document.createElement('button');
                     copyButton.innerText = 'Copy to Clipboard';
                     copyButton.addEventListener('click', function () {
-                        navigator.clipboard.writeText(data.reversed_string)
+                        navigator.clipboard.writeText(data.masked_text)
                             .then(() => {
                                 console.log('Text copied to clipboard');
                             })
@@ -96,7 +102,7 @@ document.addEventListener('mouseup', function (event) {
                     let classifyButton = document.createElement('button');
                     classifyButton.innerText = 'Classify Text';
                     classifyButton.addEventListener('click', function () {
-                        chrome.runtime.sendMessage({ action: 'classify', text: selectedText }, function(response) {
+                        chrome.runtime.sendMessage({action: 'classify', text: selectedText}, function (response) {
                             console.log('Classification Result:', response);
                             alert(`Classification Result: ${JSON.stringify(response)}`);
                         });
@@ -166,20 +172,4 @@ function makeDraggable(element) {
     }
 }
 
-//TODO
-//    [X] 1. 修改 bubble 的 click 事件，改為呼叫 FAKE API 取得 response，並顯示在對話框內
-//    [X] 2. bubble hover 時標亮
-//    [X] 3. 讓對話框 (dialogBox) 及 bubble 接可被拖曳移動
-//    [X] 4. 新增對話框內 (dialogBox) 關閉鍵，可關閉對話框
-//    [X] 5. 對話框請使用singleton設計，即一個視窗只能有一個對話框
-//    [X] 6. 泡泡與對話框按以下規則進行呈現：
-//           - 點擊泡泡：出現對話框，泡泡消失，即泡泡與對話框不會同時存在
-//           - 只有泡泡存在 + 點擊泡泡外的地方：泡泡消失
-//           - 只有對話框存在 + 點擊關閉鍵以外的任何地方：不做任何事
-//           - 只有對話框存在 + 點擊關閉鍵：對話框消失
-//    [X] 7. 限制對話框 (dialogBox) 的最大寬度、最大高度、最小寬度、最小高度
-//    [X] 8. 新增複製鍵，可讓對話框內 (dialogBox) 的文字可一鍵複製至剪貼簿
-//    [ ] 9. 導入NER model，讓機敏文字可被替換成 masked text
-//    [ ] 9. 新增機敏文字 mask前 (key)及 mask後 (value)的 reference map 至 session storage，以便後續復原原文使用
-//    [ ] 10. 新增按鈕可讓使用者將 masked text 按 session storage 內的 reference map 轉換回原文字
 
